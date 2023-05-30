326a2fc19848799459bb3f53329412987755425e
Rafael CF Lima <rafaelcflima@gmail.com>
2023-05-30 02:05:27 -0400

    Delete backend/api/config directory

--- backend/api/config/config_prod.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  mailgun: {
-    key: process.env.mailgunKey,
-    domain: process.env.mailgunDomain,
-    defaultEmail: process.env.mailgunDefaultEmail
-  }
-};
